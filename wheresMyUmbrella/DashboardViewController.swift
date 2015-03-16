19f530a05014b110d1a73a33322056084d26b741
Guilherme Akio Sakae <akio.xd@gmail.com>
2015-03-16 14:45:47 -0300

    added outlets to home screen

--- wheresMyUmbrella/DashboardViewController.swift
+++ wheresMyUmbrella/DashboardViewController.swift
@@ -9,9 +9,32 @@
 import UIKit
 
 class DashboardViewController: UIViewController {
+    
+    
+    @IBOutlet weak var guessLabel: UILabel!
+    @IBOutlet weak var todayBigIcoImageView: UIImageView!
+    @IBOutlet weak var todayPrecipChangeLabel: UILabel!
+    
+    @IBOutlet weak var dayPlusOneLabel: UILabel!
+    @IBOutlet weak var dayPlusOnePrecipChanceLabel: UILabel!
+    @IBOutlet weak var dayPlusTwoLabel: UILabel!
+    @IBOutlet weak var dayPlusTwoPrecipChanceLabel: UILabel!
+    @IBOutlet weak var dayPlusThreeLabel: UILabel!
+    @IBOutlet weak var dayPlusThreePrecipChanceLabel: UILabel!
+    @IBOutlet weak var dayPlusFourLabel: UILabel!
+    @IBOutlet weak var dayPlusFourPrecipChanceLabel: UILabel!
+    @IBOutlet weak var dayPlusFiveLabel: UILabel!
+    @IBOutlet weak var dayPlusFicePrecipChanceLabel: UILabel!
+    
+    @IBOutlet weak var wheatherLocationLabel: UILabel!
+    @IBOutlet weak var reminderTimeLabel: UILabel!
 
+    @IBOutlet weak var settingsButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
     }
 }
 
