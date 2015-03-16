caadc9afaef61d7160815c15a33b88131192b927
Guilherme Akio Sakae <akio.xd@gmail.com>
2015-03-16 14:40:30 -0300

    Added static home screen

---
+++ wheresMyUmbrella/DashboardViewController.swift
@@ -0,0 +1,17 @@
+//
+//  DashboardViewController.swift
+//  wheresMyUmbrella
+//
+//  Created by Guilherme Sakae on 2015-03-16.
+//  Copyright (c) 2015 Guilherme Sakae. All rights reserved.
+//
+
+import UIKit
+
+class DashboardViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
